39d553b60bb216b8d4585dfd206224dd155f0b44
Peng <peng37@tutanota.com>
2024-08-24 10:07:57 +0200

    turn image to png also for share

--- src/lib/shareFile.js
+++ src/lib/shareFile.js
@@ -4,37 +4,9 @@ import { saveAs } from 'file-saver';
 import { fileTypes } from './constants';
 import { convertImageTo } from './convertImage';
 
-export async function shareFile(file) {
-  try {
-    await navigator.share({
-      files: [file],
-      title: 'notenote.cc',
-    });
-    return true;
-  } catch (error) {
-    console.log('Error sharing file:', error);
-    return false;
-  }
-}
-
 export async function downloadFileWithUrl(url, type) {
   try {
-    const response = await fetch(url, { mode: 'cors' });
-    const blob = await response.blob();
-    const fileName = `notenote-${format(new Date(), 'yyyy-MM-dd-HH-mm-ss')}`;
-
-    let file;
-    if (type.includes('image')) {
-      const pngBlob = await convertImageTo(blob, fileTypes.png);
-      file = new File([pngBlob], `${fileName}.png`, {
-        type: fileTypes.png,
-      });
-    } else {
-      const fileSuffix = fileTypeToSuffix(type);
-      file = new File([blob], `${fileName}.${fileSuffix}`, {
-        type,
-      });
-    }
+    const file = await fetchFileWithUrl(url, type);
 
     saveAs(file);
   } catch (e) {
@@ -45,14 +17,7 @@ export async function downloadFileWithUrl(url, type) {
 
 export async function shareFileWithUrl(url, type) {
   try {
-    const response = await fetch(url, { mode: 'cors' });
-    const blob = await response.blob();
-    const fileSuffix = fileTypeToSuffix(type);
-    const fileName = `notenote-${format(new Date(), 'yyyy-MM-dd-HH-mm-ss')}`;
-
-    const file = new File([blob], `${fileName}.${fileSuffix}`, {
-      type,
-    });
+    const file = fetchFileWithUrl(url, type);
 
     return await shareFile(file);
   } catch (e) {
@@ -67,6 +32,40 @@ export function supportShare() {
 
 export function blobToFile(blob, name, type) {
   return new File([blob], name, { type });
+}
+
+async function fetchFileWithUrl(url, type) {
+  const response = await fetch(url, { mode: 'cors' });
+  const blob = await response.blob();
+  const fileName = `notenote-${format(new Date(), 'yyyy-MM-dd-HH-mm-ss')}`;
+
+  let file;
+  if (type.includes('image')) {
+    const pngBlob = await convertImageTo(blob, fileTypes.png);
+    file = new File([pngBlob], `${fileName}.png`, {
+      type: fileTypes.png,
+    });
+  } else {
+    const fileSuffix = fileTypeToSuffix(type);
+    file = new File([blob], `${fileName}.${fileSuffix}`, {
+      type,
+    });
+  }
+
+  return file;
+}
+
+async function shareFile(file) {
+  try {
+    await navigator.share({
+      files: [file],
+      title: 'notenote.cc',
+    });
+    return true;
+  } catch (error) {
+    console.log('Error sharing file:', error);
+    return false;
+  }
 }
 
 function fileTypeToSuffix(type) {
